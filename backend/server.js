ddc4031e0c0a130eb9d4622ee1c230a027e74e4b
Mohitjadav13 <145259738+Mohitjadav13@users.noreply.github.com>
2024-03-29 21:42:03 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const eventRoutes = require('./routes/eventRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://event:event@cluster0.0bxegcp.mongodb.net/event?retryWrites=true&w=majority&appName=Cluster0', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => {
+	console.log('Connected to MongoDB')
+});
+
+// Routes
+app.use('/api/events', eventRoutes);
+
+// Start server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
